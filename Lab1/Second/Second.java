e6b0f3947f781bfbd7c4575739a6f2c67d153731
corradowaver <corradowave1@gmail.com>
2019-10-01 22:25:40 +0300

    friendly ui

--- Lab1/Second/Second.java
+++ Lab1/Second/Second.java
@@ -4,17 +4,24 @@ import java.util.Scanner;
 
 public class Second {
   static String message = "none";
+  static Scanner scan = new Scanner(System.in);
 
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+
     while (message.length() % 3 != 0) {
+     System.out.println("Enter string (length must be divided by 3 with 0 remainder)");
       message = scan.next();
     }
+
     String[] newArray = separation(message);
     for (int i = 0; i < newArray.length; i++) {
       newArray[i] = randMidChar(newArray[i]);
     }
     Arrays.sort(newArray);
+
+    for (String x : newArray) {
+      System.out.println(x);
+    }
     scan.close();
   }
 
@@ -24,16 +31,13 @@ public class Second {
 
   public static String randMidChar(String message) {
     StringBuilder newMessage = new StringBuilder(message);
-    newMessage.setCharAt(1, randomChar(newMessage.charAt(1)));
-    return newMessage.toString();
-  }
-
-  public static char randomChar(char a) {
     Random rnd = new Random();
-    char c = a;
-    while (a == c) {
-      c = (char) (rnd.nextInt(26) + 'a');
+    char newChar = '0';
+    while (newChar == newMessage.charAt(0) || newChar == newMessage.charAt(1) 
+      || newChar == newMessage.charAt(2) || newChar == '0') {
+      newChar = (char) (rnd.nextInt(26) + 'a');
     }
-    return c;
+    newMessage.setCharAt(1, newChar);
+    return newMessage.toString();
   }
 }

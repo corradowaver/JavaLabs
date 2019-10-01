dc3c7e13693a25bc072f62ad5b4649e28569d14c
corradowaver <corradowave1@gmail.com>
2019-10-01 22:24:55 +0300

    Some fixes

--- Lab1/First/First.java
+++ Lab1/First/First.java
@@ -1,20 +1,33 @@
 import java.util.Scanner;
 
 public class First {
-  static int lines_;
-  static int colomns_;
+  static int lines;
+  static int columns;
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    lines_ = scan.nextInt();
-    colomns_ = scan.nextInt();
+    lines = -1;
+    columns = -1;
 
-    int[][] matrix = new int[lines_][colomns_];
-    for (int i = 0; i < lines_; i++) {
+    while (lines < 0 || columns < 0) {
+      System.out.println("Enter lines then colomns (must be > 0)");
+      lines = scan.nextInt();
+      columns = scan.nextInt();
+    }
+
+    int[][] matrix = new int[lines][columns];
+    for (int i = 0; i < lines; i++) {
       matrix[i][0] = 1;
-      for (int j = 1; j < colomns_; j++) {
+      for (int j = 1; j < columns; j++) {
         matrix[i][j] = 0;
       }
+    }
+
+    for (int i = 0; i < lines; i++) {
+      for (int j = 0; j < columns; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
     }
 
     scan.close();

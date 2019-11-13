04b682058f63906490951f07778ef5c084763f62
corradowaver <corradowave1@gmail.com>
2019-11-14 00:19:21 +0300

    friendly ui

--- Lab6/src/Main.java
+++ Lab6/src/Main.java
@@ -1,46 +1,44 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.concurrent.CompletableFuture;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     ArrayList<Transaction> transactionsList = Reader.getTransactionsList();
-
-    for (Transaction trans : transactionsList) {
+    transactionsList.forEach(trans -> {
       System.out.println(String.format("Счет отправителя: %s, Счет получателя: %s, Денюшки %s",
           trans.getSenderAccount(), trans.getGetterAccount(), trans.getMoneyAmount()));
-    }
+    });
+
+    Account moneyDealer = new Account(99999999, 999999);
+    Account mrGetter1 = new Account(11111111, 0);
+    Account mrGetter2 = new Account(22222222, 0);
+    Account mrGetter3 = new Account(33333333, 0);
+    Account mrGetter4 = new Account(44444444, 0);
 
     HashMap<Long, Account> accountsList = new HashMap<Long, Account>();
-    Account mrSender = new Account(88888888, 1000);
-    Account mrGetter = new Account(44444444, 0);
-    accountsList.put(mrSender.getId(), mrSender);
-    accountsList.put(mrGetter.getId(), mrGetter);
+
+    accountsList.put(moneyDealer.getId(), moneyDealer);
+    accountsList.put(mrGetter1.getId(), mrGetter1);
+    accountsList.put(mrGetter2.getId(), mrGetter2);
+    accountsList.put(mrGetter3.getId(), mrGetter3);
+    accountsList.put(mrGetter4.getId(), mrGetter4);
 
     Handler handler = new Handler();
     handler.setAccountsList(accountsList);
 
-    System.out.println(handler.getAccountsList()
-        .get(mrSender.getId())
-        .getMoney()
-    );
-    System.out.println(handler.getAccountsList()
-        .get(mrGetter.getId())
-        .getMoney()
-    );
+    System.out.println("\n! Before handling all transactions: !");
+    handler.getAccountsList().forEach((id, account) -> {
+      System.out.println("__________\nid: " + id + "\nMoney: " + account.getMoney());
+    });
 
     handler.startHandling();
 
-    CompletableFuture.runAsync(() -> {
-      System.out.println(handler.getAccountsList()
-          .get(mrSender.getId())
-          .getMoney()
-      );
-      System.out.println(handler.getAccountsList()
-          .get(mrGetter.getId())
-          .getMoney()
-      );
+    Thread.sleep(1000);
+    System.out.println("\n! After handling all transactions: !");
+    handler.getAccountsList().forEach((id, account) -> {
+      System.out.println("__________\nid: " + id + "\nMoney: " + account.getMoney());
     });
+
   }
 }
